69efd5557351355f815f6b93f6cd292d7bb46e92
mfina94 <matthawkeyefina@gmail.com>
2016-02-29 21:37:10 -0600

    communicating with db

--- hawk_relief/application/controllers/main.php
+++ hawk_relief/application/controllers/main.php
@@ -4,9 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller
 {
+	public function __construct() {
+		parent::__construct();
+	}
 	
 	public function index() {
+		$this->load->model('user');
+		$this->user->test();
 		$this->load->view('homepage');
         #asdfasdfasdf
+	}
+	
+	public function signup() {
+		
 	}
 }
